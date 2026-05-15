8d0ed0c16b931c67af1fbdac4adf714a55dd2d64
naufalbintang <naufalbintangpradana@gmail.com>
2026-05-15 17:26:59 +0700

    menambahkan fitur enhancement: histogram equalization

--- backend/routers/enhancement.py
+++ backend/routers/enhancement.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, File, UploadFile, Form, Response
 import os
 
 from utils.image_utils import *
-from services.enhancement import brightness_contrast
+from services.enhancement import brightness_contrast, histogram_equalization
 
 router = APIRouter()
 
@@ -26,4 +26,24 @@ async def brightness_contrast_endpoint(
     if media_t == "image/jpg":
         media_t = "image/jpeg"
     
+    return Response(content=output_bytes, media_type=media_t)
+
+@router.post("/api/enhancement/histogram-equalization")
+async def histogram_equalization_endpoint(
+    file: UploadFile = File(...)
+):
+    _, ext = os.path.splitext(file.filename)
+    if not ext:
+        ext = ".jpg"
+        
+    img = await read_image_as_array(file)
+    
+    hasil_manipulasi = histogram_equalization(img)
+
+    output_bytes = encode_image_to_bytes(hasil_manipulasi, extension=ext)
+    
+    media_t = f"image/{ext.replace('.', '')}"
+    if media_t == "image/jpg":
+        media_t = "image/jpeg"
+        
     return Response(content=output_bytes, media_type=media_t)
--- backend/services/enhancement.py
+++ backend/services/enhancement.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 
 
 def brightness_contrast(img: np.ndarray, brightness: int, contrast: float) -> np.ndarray:
@@ -16,3 +17,35 @@ def brightness_contrast(img: np.ndarray, brightness: int, contrast: float) -> np
     res_final = res_clipped.astype(np.uint8)
     
     return res_final
+
+def histogram_equalization(img: np.ndarray) -> np.ndarray:
+    
+    # ubah warna dari format BGR ke HSV
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # pisahkan h=warna, s=kepekatan, v=kecerahan/intensitas
+    h, s, v = cv2.split(hsv)
+    
+    # hitung histogram manual menggunakan numpy
+    hist, bins = np.histogram(v.flatten(), 256, [0, 256])
+    
+    # hitung cdf
+    cdf = hist.cumsum()
+    
+    # normalisasi cdf, gunakan masking untuk mengabaikan nilai 0 agar pembagian tidak error
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    
+    # isi kembali yang di-mask dengan angka 0
+    cdf_final = np.ma.filled(cdf_m, 0).astype(np.uint8)
+    
+    # pemetaan nilai piksel lama ke nilai piksel baru berdasarkan index cdf
+    v_eq = cdf_final[v]
+    
+    # gabungkan kembali channel H, S, V yang sudah di-equalize
+    hsv_eq = cv2.merge((h, s, v_eq))
+    
+    # kembalikan ke format BGR
+    res_final = cv2.cvtColor(hsv_eq, cv2.COLOR_HSV2BGR)
+    
+    return res_final
+    
